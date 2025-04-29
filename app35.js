bc9ed9f2a6195406c32759ce59e7a63c4d9089a1
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-29 19:15:57 +0530

    Add files via upload

---
+++ app35.js
@@ -0,0 +1,21 @@
+// for (let i = 2; i <= 10000; i = i + 2) {
+//     console.log(i);
+// }
+
+// console.log("backwards");
+
+// for(let i = 10; i>= 2; i = i - 2) {
+//     console.log(i);
+// }
+let n = prompt("Write your number");
+n = parseInt(n);
+
+for(let i=n; i<=n*10; i=i+n) {
+    console.log(i);
+}
+
+
+
+// for(let i = 3; i<= 30; i = i + 3) {
+//     console.log(i);
+// }
